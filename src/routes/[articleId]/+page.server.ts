bd7607acccc34417eac6bc3ffaf7e1413d00a9c4
Paul Valdez <18030566@itcelaya.edu.mx>
2024-09-02 09:46:34 -0600

    Actualización, tema oscuro fijado y CRUD arreglado
    Se ha hecho que la pagina siempre esté en modo oscuro o dark y el crud ha sido arreglado(el update)

--- src/routes/[articleId]/+page.server.ts
+++ src/routes/[articleId]/+page.server.ts
@@ -3,20 +3,16 @@ import { prisma } from "$lib/server/prisma";
 import { error, fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ params }) => {
-	const getArticle = async () => {
-		const article = await prisma.article.findUnique({
-			where: {
-				id: Number(params.articleId),
-			},
-		});
-		if (!article) {
-			throw error(404, "Article not found");
-		}
-		return article;
-	};
-
+	const article = await prisma.article.findUnique({
+		where: {
+			id: Number(params.articleId),
+		},
+	});
+	if (!article) {
+		throw error(404, "Article not found");
+	}
 	return {
-		article: getArticle(),
+		article,
 	};
 };
 
